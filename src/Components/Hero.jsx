866f6edbcd63df6d41a44d61f02ce91b84555e96
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-10-17 23:37:34 +0600

    fix footer issue

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,14 +3,15 @@ const Hero = () => {
     <section className=" text-white">
       <div className="mx-auto max-w-screen-xl px-4 py-10 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
-          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-            Understand User Flow.
-            <span className="sm:block"> Increase Conversion. </span>
+          <h1 className="bg-gradient-to-r from-green-300  via-blue-500 hover:animate-pulse to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+            SMTP Mails system using nodemailer and react.js
           </h1>
 
-          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed text-black ">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+          <p className="mx-auto mt-4 max-w-xl  text-black ">
+            With this setup, you can capture email data in your React app and
+            send it to your backend server, which will handle the email sending
+            using Nodemailer. This approach allows you to manage email
+            functionalities efficiently within your application.
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import { Toaster } from "react-hot-toast";
-// import Footer from "../../Footer/Footer";
+import Footer from "../Footer/Footer";
 
 
 const Layout = () => {
@@ -9,7 +9,7 @@ const Layout = () => {
         <div className="capitalize">
             <Navbar></Navbar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
             <Toaster position="top-center" reverseOrder={false} />
             
         </div>
